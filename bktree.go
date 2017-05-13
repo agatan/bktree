a4fd8a87a6aa4acbc159d04b69c54730950ee55a
agatan <agatan039@gmail.com>
2017-05-13 15:47:03 +0900

    Use slice instead of map to hold children

--- bktree.go
+++ bktree.go
@@ -6,7 +6,26 @@ type BKTree struct {
 
 type node struct {
 	entry    Entry
-	children map[int]*node
+	children []struct {
+		distance int
+		node     *node
+	}
+}
+
+func (n *node) addChild(e Entry) {
+	newnode := &node{entry: e}
+loop:
+	d := n.entry.Distance(e)
+	for _, c := range n.children {
+		if c.distance == d {
+			n = c.node
+			goto loop
+		}
+	}
+	n.children = append(n.children, struct {
+		distance int
+		node     *node
+	}{d, newnode})
 }
 
 type Entry interface {
@@ -16,24 +35,11 @@ type Entry interface {
 func (bk *BKTree) Add(entry Entry) {
 	if bk.root == nil {
 		bk.root = &node{
-			entry:    entry,
-			children: make(map[int]*node),
+			entry: entry,
 		}
 		return
 	}
-	nd := bk.root
-	for {
-		d := nd.entry.Distance(entry)
-		if next, ok := nd.children[d]; ok {
-			nd = next
-			continue
-		}
-		nd.children[d] = &node{
-			entry:    entry,
-			children: make(map[int]*node),
-		}
-		return
-	}
+	bk.root.addChild(entry)
 }
 
 type Result struct {
@@ -59,9 +65,9 @@ func (bk *BKTree) Search(needle Entry, tolerance int) []*Result {
 		}
 
 		low, high := d-tolerance, d+tolerance
-		for k, child := range c.children {
-			if low <= k && k <= high {
-				candidates = append(candidates, child)
+		for _, c := range c.children {
+			if low <= c.distance && c.distance <= high {
+				candidates = append(candidates, c.node)
 			}
 		}
 	}
